804edd72ab5033bdcbc63b38c2ed18a6116a9c48
Enjih <71701397@aluno.faculdadecotemig.br>
2019-10-24 19:17:27 -0300

    Criação do projeto

---
+++ src/test/java/com/cotemig/CampGest/CampGestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotemig.CampGest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampGestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
